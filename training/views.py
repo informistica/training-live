0eaca184ed4a25ae7951df00623cff2128b2501a
informistica <mauro.spinarelli@gmail.com>
2020-04-22 10:58:31 +0200

    update

--- training/views.py
+++ training/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 def home(request): 
-    blog_command = ['esempi','blog']
+    applicazioni = ['esempi','blog']
     context = {
-        'menu': blog_command,
+        'menu': applicazioni,
     }
     return render(request, "index.html", context)

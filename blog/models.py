1a297b5215e72f327a7fa1c181f0d00b2bcf508a
informistica <mauro.spinarelli@gmail.com>
2020-04-23 11:53:44 +0200

    db

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
  
 # Create your models here.
 class BlogPostModel(models.Model):
     titolo = models.CharField(max_length=100)
     contenuto = models.TextField()
     bozza = models.BooleanField()
+    data_creazione = models.DateTimeField(auto_now_add=True)
+    autore = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
 
     def __str__(self):
         return self.titolo

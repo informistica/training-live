f463cc26228591d00bbb996a7e4f40a2eb74267d
informistica <mauro.spinarelli@gmail.com>
2020-05-18 08:27:49 +0200

    venv

--- utenti/forms.py
+++ utenti/forms.py
@@ -8,7 +8,7 @@ class FormRegistrazione(UserCreationForm):
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name','last_name','username', 'email', 'password1', 'password2']
         #fields = "__all__"
 
 
